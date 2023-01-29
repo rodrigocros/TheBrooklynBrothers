4fca820a5fab1bb765441d804e05a1f24278e2e1
Rodrigo Rosa <rodrigorosa.dev@gmail.com>
2023-01-29 18:22:06 -0300

    feature-site-reponsive

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const menu = document.querySelector('#menu-btn');
 menu.addEventListener('click', () =>{
     document.querySelector('.options').classList.toggle('active');
     document.querySelector('.options').classList.toggle('close');
-    document.querySelector('#menu-btn').classList.remove('fa-bars');
-    document.querySelector('#menu-btn').classList.add('fa-close');
-
+    document.querySelector('#menu-btn').classList.toggle('fa-bars');
+    document.querySelector('#menu-btn').classList.toggle('fa-close');
 });
+
