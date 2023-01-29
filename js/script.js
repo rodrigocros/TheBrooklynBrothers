d1906efa38af370cd463a5f01f5d2e6e9492cfee
Rodrigo Rosa <rodrigorosa.dev@gmail.com>
2023-01-29 17:00:30 -0300

    feature-header-responsive-done

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const menu = document.querySelector('#menu-btn');
 
 
 menu.addEventListener('click', () =>{
-    document.querySelector('.navbaar').classList.toggle('active');
-    document.querySelector('.navbaar').classList.toggle('close');
+    document.querySelector('.options').classList.toggle('active');
+    document.querySelector('.options').classList.toggle('close');
     document.querySelector('#menu-btn').classList.remove('fa-bars');
     document.querySelector('#menu-btn').classList.add('fa-close');
 
