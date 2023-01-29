5ff39029b2de256d991a2bc322fe00aaf50e9c65
Rodrigo Rosa <rodrigorosa.dev@gmail.com>
2023-01-29 01:27:09 -0300

    feature-start

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const menu = document.querySelector('#menu-btn');
 
 
 menu.addEventListener('click', () =>{
-    document.querySelector('.navbar').classList.toggle('active');
-    document.querySelector('.navbar').classList.toggle('close');
+    document.querySelector('.navbaar').classList.toggle('active');
+    document.querySelector('.navbaar').classList.toggle('close');
     document.querySelector('#menu-btn').classList.remove('fa-bars');
     document.querySelector('#menu-btn').classList.add('fa-close');
 
